7cebbbfe276f5f8c7f6fa721880aeab2db0ae315
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-25 21:54:01 -0800

    wrote a useeffect and set state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useEffect } from 'react';
 import { fetchHolidays } from './services/holidays';
+import { useState } from 'react';
 
 export default function App() {
-  useEffect(async () => {
-    await fetchHolidays();
-    console.log(await fetchHolidays());
-  });
+  const [loading, setLoading] = useState(true);
+  const [holiday, setHoliday] = useState('');
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchHolidays();
+      setHoliday(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+  if (loading) return <h1>loading...</h1>;
   return <h1>Hello World</h1>;
 }
