af2e7fd8f0d582c6c8963fa4b9b976a526cad464
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-28 12:47:17 -0800

    made insignificant changes just tryin to switch branches

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//component test
 test('should render a header', async () => {
   render(<App />);
   const header = await screen.findByRole('heading', { name: /us holidays/i });
   expect(header).toBeInTheDocument();
 });
+
+//behavioral test
+test('');
