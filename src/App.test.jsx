e32e8767706a7616055d0fcb5475539b31ce7bc8
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-28 13:02:26 -0800

    added behavior tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 //component test
 test('should render a header', async () => {
@@ -9,4 +10,23 @@ test('should render a header', async () => {
 });
 
 //behavioral test
-test('');
+
+test('should display user search returning one matching item correctly', async () => {
+  render(<App />);
+  const search = await screen.findByRole('textbox');
+  const button = await screen.findByRole('button');
+  userEvent.type(search, `New Year's Day`);
+  userEvent.click(button);
+  await waitFor(() => expect(screen.getAllByRole('heading')).toHaveLength(4));
+});
+
+test('should display multiples if matches user search', async () => {
+  render(<App />);
+  const search = await screen.findByRole('textbox');
+  const button = await screen.findByRole('button');
+  userEvent.type(search, `in`);
+  userEvent.click(button);
+  await waitFor(() => expect(screen.getAllByRole('heading')).toHaveLength(13), {
+    timeout: 5000,
+  });
+});
